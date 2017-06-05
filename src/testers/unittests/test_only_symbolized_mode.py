f38dfdc0695f31e8ae88e75a4ba5d8b89f8a0c78
Jonathan Salwan <jonathan.salwan@gmail.com>
2017-06-05 14:46:14 +0200

    try thing

--- src/testers/unittests/test_only_symbolized_mode.py
+++ src/testers/unittests/test_only_symbolized_mode.py
@@ -39,135 +39,135 @@ class TestOnlySymbolizedMode(unittest.TestCase):
         self.assertEqual(len(inst.getLoadAccess()), 0)
         self.assertEqual(len(inst.getStoreAccess()), 0)
 
-    def test_2(self):
-        ctx = TritonContext()
-        ctx.setArchitecture(ARCH.X86_64)
-        ctx.enableMode(MODE.ONLY_ON_SYMBOLIZED, True)
-        ctx.convertRegisterToSymbolicVariable(ctx.Register(REG.X86_64.RAX))
+    #def test_2(self):
+    #    ctx = TritonContext()
+    #    ctx.setArchitecture(ARCH.X86_64)
+    #    ctx.enableMode(MODE.ONLY_ON_SYMBOLIZED, True)
+    #    ctx.convertRegisterToSymbolicVariable(ctx.Register(REG.X86_64.RAX))
 
-        inst = Instruction("\x48\x89\xc3") # mov rbx, rax
-        ctx.processing(inst)
+    #    inst = Instruction("\x48\x89\xc3") # mov rbx, rax
+    #    ctx.processing(inst)
 
-        self.assertEqual(len(inst.getReadRegisters()), 1)
-        self.assertEqual(len(inst.getWrittenRegisters()), 1)
-        self.assertEqual(len(inst.getLoadAccess()), 0)
-        self.assertEqual(len(inst.getStoreAccess()), 0)
+    #    self.assertEqual(len(inst.getReadRegisters()), 1)
+    #    self.assertEqual(len(inst.getWrittenRegisters()), 1)
+    #    self.assertEqual(len(inst.getLoadAccess()), 0)
+    #    self.assertEqual(len(inst.getStoreAccess()), 0)
 
-        try:
-            str(inst.getReadRegisters()[0][1])
-            str(inst.getWrittenRegisters()[0][1])
-        except:
-            self.fail("test_2() raised unexpectedly!")
+    #    try:
+    #        str(inst.getReadRegisters()[0][1])
+    #        str(inst.getWrittenRegisters()[0][1])
+    #    except:
+    #        self.fail("test_2() raised unexpectedly!")
 
-    def test_3(self):
-        ctx = TritonContext()
-        ctx.setArchitecture(ARCH.X86_64)
+    #def test_3(self):
+    #    ctx = TritonContext()
+    #    ctx.setArchitecture(ARCH.X86_64)
 
-        inst = Instruction("\x48\x8b\x18") # mov rbx, qword ptr [rax]
-        ctx.processing(inst)
+    #    inst = Instruction("\x48\x8b\x18") # mov rbx, qword ptr [rax]
+    #    ctx.processing(inst)
 
-        self.assertEqual(len(inst.getReadRegisters()), 1)
-        self.assertEqual(len(inst.getWrittenRegisters()), 2)
-        self.assertEqual(len(inst.getLoadAccess()), 1)
-        self.assertEqual(len(inst.getStoreAccess()), 0)
+    #    self.assertEqual(len(inst.getReadRegisters()), 1)
+    #    self.assertEqual(len(inst.getWrittenRegisters()), 2)
+    #    self.assertEqual(len(inst.getLoadAccess()), 1)
+    #    self.assertEqual(len(inst.getStoreAccess()), 0)
 
-        try:
-            str(inst.getReadRegisters()[0][1])
-            str(inst.getWrittenRegisters()[0][1])
-            str(inst.getWrittenRegisters()[1][1])
-            str(inst.getLoadAccess()[0][1])
-        except:
-            self.fail("test_3() raised unexpectedly!")
+    #    try:
+    #        str(inst.getReadRegisters()[0][1])
+    #        str(inst.getWrittenRegisters()[0][1])
+    #        str(inst.getWrittenRegisters()[1][1])
+    #        str(inst.getLoadAccess()[0][1])
+    #    except:
+    #        self.fail("test_3() raised unexpectedly!")
 
-    def test_4(self):
-        ctx = TritonContext()
-        ctx.setArchitecture(ARCH.X86_64)
-        ctx.enableMode(MODE.ONLY_ON_SYMBOLIZED, True)
-        ctx.convertRegisterToSymbolicVariable(ctx.Register(REG.X86_64.RAX))
+    #def test_4(self):
+    #    ctx = TritonContext()
+    #    ctx.setArchitecture(ARCH.X86_64)
+    #    ctx.enableMode(MODE.ONLY_ON_SYMBOLIZED, True)
+    #    ctx.convertRegisterToSymbolicVariable(ctx.Register(REG.X86_64.RAX))
 
-        inst = Instruction("\x48\x8b\x18") # mov rbx, qword ptr [rax]
-        ctx.processing(inst)
+    #    inst = Instruction("\x48\x8b\x18") # mov rbx, qword ptr [rax]
+    #    ctx.processing(inst)
 
-        self.assertEqual(len(inst.getReadRegisters()), 1)
-        self.assertEqual(len(inst.getWrittenRegisters()), 0)
-        self.assertEqual(len(inst.getLoadAccess()), 0)
-        self.assertEqual(len(inst.getStoreAccess()), 0)
+    #    self.assertEqual(len(inst.getReadRegisters()), 1)
+    #    self.assertEqual(len(inst.getWrittenRegisters()), 0)
+    #    self.assertEqual(len(inst.getLoadAccess()), 0)
+    #    self.assertEqual(len(inst.getStoreAccess()), 0)
 
-        try:
-            str(inst.getReadRegisters()[0][1])
-        except:
-            self.fail("test_4() raised unexpectedly!")
+    #    try:
+    #        str(inst.getReadRegisters()[0][1])
+    #    except:
+    #        self.fail("test_4() raised unexpectedly!")
 
-    def test_5(self):
-        ctx = TritonContext()
-        ctx.setArchitecture(ARCH.X86_64)
-        ctx.enableMode(MODE.ONLY_ON_SYMBOLIZED, True)
-        ctx.convertMemoryToSymbolicVariable(MemoryAccess(0, CPUSIZE.QWORD))
+    #def test_5(self):
+    #    ctx = TritonContext()
+    #    ctx.setArchitecture(ARCH.X86_64)
+    #    ctx.enableMode(MODE.ONLY_ON_SYMBOLIZED, True)
+    #    ctx.convertMemoryToSymbolicVariable(MemoryAccess(0, CPUSIZE.QWORD))
 
-        inst = Instruction("\x48\x8b\x18") # mov rbx, qword ptr [rax]
-        ctx.processing(inst)
+    #    inst = Instruction("\x48\x8b\x18") # mov rbx, qword ptr [rax]
+    #    ctx.processing(inst)
 
-        self.assertEqual(len(inst.getReadRegisters()), 0)
-        self.assertEqual(len(inst.getWrittenRegisters()), 1)
-        self.assertEqual(len(inst.getLoadAccess()), 1)
-        self.assertEqual(len(inst.getStoreAccess()), 0)
+    #    self.assertEqual(len(inst.getReadRegisters()), 0)
+    #    self.assertEqual(len(inst.getWrittenRegisters()), 1)
+    #    self.assertEqual(len(inst.getLoadAccess()), 1)
+    #    self.assertEqual(len(inst.getStoreAccess()), 0)
 
-        try:
-            str(inst.getWrittenRegisters()[0][1])
-            str(inst.getLoadAccess()[0][1])
-        except:
-            self.fail("test_5() raised unexpectedly!")
+    #    try:
+    #        str(inst.getWrittenRegisters()[0][1])
+    #        str(inst.getLoadAccess()[0][1])
+    #    except:
+    #        self.fail("test_5() raised unexpectedly!")
 
-    def test_6(self):
-        ctx = TritonContext()
-        ctx.setArchitecture(ARCH.X86_64)
-        ctx.enableMode(MODE.ONLY_ON_SYMBOLIZED, True)
-        ctx.convertRegisterToSymbolicVariable(ctx.Register(REG.X86_64.RAX))
-        ctx.convertMemoryToSymbolicVariable(MemoryAccess(0, CPUSIZE.QWORD))
+    #def test_6(self):
+    #    ctx = TritonContext()
+    #    ctx.setArchitecture(ARCH.X86_64)
+    #    ctx.enableMode(MODE.ONLY_ON_SYMBOLIZED, True)
+    #    ctx.convertRegisterToSymbolicVariable(ctx.Register(REG.X86_64.RAX))
+    #    ctx.convertMemoryToSymbolicVariable(MemoryAccess(0, CPUSIZE.QWORD))
 
-        inst = Instruction("\x48\x8b\x18") # mov rbx, qword ptr [rax]
-        ctx.processing(inst)
+    #    inst = Instruction("\x48\x8b\x18") # mov rbx, qword ptr [rax]
+    #    ctx.processing(inst)
 
-        self.assertEqual(len(inst.getReadRegisters()), 1)
-        self.assertEqual(len(inst.getWrittenRegisters()), 1)
-        self.assertEqual(len(inst.getLoadAccess()), 1)
-        self.assertEqual(len(inst.getStoreAccess()), 0)
+    #    self.assertEqual(len(inst.getReadRegisters()), 1)
+    #    self.assertEqual(len(inst.getWrittenRegisters()), 1)
+    #    self.assertEqual(len(inst.getLoadAccess()), 1)
+    #    self.assertEqual(len(inst.getStoreAccess()), 0)
 
-        try:
-            str(inst.getReadRegisters()[0][1])
-            str(inst.getWrittenRegisters()[0][1])
-            str(inst.getLoadAccess()[0][1])
-        except:
-            self.fail("test_6() raised unexpectedly!")
+    #    try:
+    #        str(inst.getReadRegisters()[0][1])
+    #        str(inst.getWrittenRegisters()[0][1])
+    #        str(inst.getLoadAccess()[0][1])
+    #    except:
+    #        self.fail("test_6() raised unexpectedly!")
 
-    def test_7(self):
-        ctx = TritonContext()
-        ctx.setArchitecture(ARCH.X86_64)
-        ctx.enableMode(MODE.ONLY_ON_SYMBOLIZED, True)
-        ctx.setConcreteRegisterValue(ctx.Register(REG.X86_64.RAX, 0x1337))
+    #def test_7(self):
+    #    ctx = TritonContext()
+    #    ctx.setArchitecture(ARCH.X86_64)
+    #    ctx.enableMode(MODE.ONLY_ON_SYMBOLIZED, True)
+    #    ctx.setConcreteRegisterValue(ctx.Register(REG.X86_64.RAX, 0x1337))
 
-        inst = Instruction("\x48\x8b\x18") # mov rbx, qword ptr [rax]
-        ctx.processing(inst)
+    #    inst = Instruction("\x48\x8b\x18") # mov rbx, qword ptr [rax]
+    #    ctx.processing(inst)
 
-        self.assertEqual(inst.getOperands()[1].getAddress(), 0x1337)
-        self.assertIsNone(inst.getOperands()[1].getLeaAst())
+    #    self.assertEqual(inst.getOperands()[1].getAddress(), 0x1337)
+    #    self.assertIsNone(inst.getOperands()[1].getLeaAst())
 
-    def test_8(self):
-        ctx = TritonContext()
-        ctx.setArchitecture(ARCH.X86_64)
-        ctx.enableMode(MODE.ONLY_ON_SYMBOLIZED, True)
-        ctx.setConcreteRegisterValue(ctx.Register(REG.X86_64.RAX, 0x1337))
-        ctx.convertRegisterToSymbolicVariable(ctx.Register(REG.X86_64.RAX))
-        ctx.convertMemoryToSymbolicVariable(MemoryAccess(0, CPUSIZE.QWORD))
+    #def test_8(self):
+    #    ctx = TritonContext()
+    #    ctx.setArchitecture(ARCH.X86_64)
+    #    ctx.enableMode(MODE.ONLY_ON_SYMBOLIZED, True)
+    #    ctx.setConcreteRegisterValue(ctx.Register(REG.X86_64.RAX, 0x1337))
+    #    ctx.convertRegisterToSymbolicVariable(ctx.Register(REG.X86_64.RAX))
+    #    ctx.convertMemoryToSymbolicVariable(MemoryAccess(0, CPUSIZE.QWORD))
 
-        inst = Instruction("\x48\x8b\x18") # mov rbx, qword ptr [rax]
-        ctx.processing(inst)
+    #    inst = Instruction("\x48\x8b\x18") # mov rbx, qword ptr [rax]
+    #    ctx.processing(inst)
 
-        self.assertEqual(inst.getOperands()[1].getAddress(), 0x1337)
-        self.assertIsNotNone(inst.getOperands()[1].getLeaAst())
+    #    self.assertEqual(inst.getOperands()[1].getAddress(), 0x1337)
+    #    self.assertIsNotNone(inst.getOperands()[1].getLeaAst())
 
-        try:
-            str(inst.getOperands()[1].getLeaAst())
-        except:
-            self.fail("test_8() raised unexpectedly!")
+    #    try:
+    #        str(inst.getOperands()[1].getLeaAst())
+    #    except:
+    #        self.fail("test_8() raised unexpectedly!")
 
