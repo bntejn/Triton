605052b2649f61bcdded88d5f69e842bb710b993
Jonathan Salwan <jonathan.salwan@gmail.com>
2017-06-06 14:23:42 +0200

    try thing

--- src/libtriton/arch/irBuilder.cpp
+++ src/libtriton/arch/irBuilder.cpp
@@ -156,8 +156,8 @@ namespace triton {
        * concrete expressions and their AST nodes.
        */
       if (this->symbolicEngine->isEnabled() && this->modes.isModeEnabled(triton::modes::ONLY_ON_SYMBOLIZED)) {
-        /* Clean memory operands */
-        this->collectUnsymbolizedNodes(uniqueNodes, inst.operands);
+        ///* Clean memory operands */
+        //this->collectUnsymbolizedNodes(uniqueNodes, inst.operands);
 
         /* Clean implicit and explicit semantics - MEM */
         this->collectUnsymbolizedNodes(uniqueNodes, inst.getLoadAccess());
