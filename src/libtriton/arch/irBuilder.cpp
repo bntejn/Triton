e19589ec512fa863d25d65cf2974fe7816ddfb0e
Jonathan Salwan <jonathan.salwan@gmail.com>
2017-06-07 00:59:28 +0200

    try thing

--- src/libtriton/arch/irBuilder.cpp
+++ src/libtriton/arch/irBuilder.cpp
@@ -157,22 +157,22 @@ namespace triton {
        */
       if (this->symbolicEngine->isEnabled() && this->modes.isModeEnabled(triton::modes::ONLY_ON_SYMBOLIZED)) {
         /* Clean memory operands */
-        this->collectUnsymbolizedNodes(uniqueNodes, inst.operands);
+        //this->collectUnsymbolizedNodes(uniqueNodes, inst.operands);
 
         /* Clean implicit and explicit semantics - MEM */
-        this->collectUnsymbolizedNodes(uniqueNodes, inst.getLoadAccess());
+        //this->collectUnsymbolizedNodes(uniqueNodes, inst.getLoadAccess());
 
         /* Clean implicit and explicit semantics - REG */
-        this->collectUnsymbolizedNodes(uniqueNodes, inst.getReadRegisters());
+        //this->collectUnsymbolizedNodes(uniqueNodes, inst.getReadRegisters());
 
         /* Clean implicit and explicit semantics - IMM */
-        this->collectUnsymbolizedNodes(uniqueNodes, inst.getReadImmediates());
+        //this->collectUnsymbolizedNodes(uniqueNodes, inst.getReadImmediates());
 
         /* Clean implicit and explicit semantics - MEM */
-        this->collectUnsymbolizedNodes(uniqueNodes, inst.getStoreAccess(), true);
+        //this->collectUnsymbolizedNodes(uniqueNodes, inst.getStoreAccess(), true);
 
         /* Clean implicit and explicit semantics - REG */
-        this->collectUnsymbolizedNodes(uniqueNodes, inst.getWrittenRegisters());
+        //this->collectUnsymbolizedNodes(uniqueNodes, inst.getWrittenRegisters());
 
         /* Clean symbolic expressions */
         for (auto it = inst.symbolicExpressions.begin(); it != inst.symbolicExpressions.end(); it++) {
