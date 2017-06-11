b9c893e56171534614704cd4f12ca8d2f965df36
Jonathan Salwan <jonathan.salwan@gmail.com>
2017-06-11 15:55:40 +0200

    try thing

--- src/libtriton/arch/irBuilder.cpp
+++ src/libtriton/arch/irBuilder.cpp
@@ -243,7 +243,7 @@ namespace triton {
       for (auto it = operands.begin(); it != operands.end(); it++) {
         if (it->getType() == triton::arch::OP_MEM) {
           this->astGarbageCollector.extractUniqueAstNodes(uniqueNodes, it->getMemory().getLeaAst());
-          it->getMemory().setLeaAst(nullptr);
+          //it->getMemory().setLeaAst(nullptr);
         }
       }
     }
@@ -283,7 +283,7 @@ namespace triton {
         if (it->getType() == triton::arch::OP_MEM) {
           if (it->getMemory().getLeaAst()->isSymbolized() == false) {
             //this->astGarbageCollector.extractUniqueAstNodes(uniqueNodes, it->getMemory().getLeaAst());
-            it->getMemory().setLeaAst(nullptr);
+            //it->getMemory().setLeaAst(nullptr);
           }
         }
       }
