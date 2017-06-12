74e6756f9e863c513a36775a163d83c6ae844038
Jonathan Salwan <jonathan.salwan@gmail.com>
2017-06-12 16:22:43 +0200

    try thing

--- src/libtriton/arch/irBuilder.cpp
+++ src/libtriton/arch/irBuilder.cpp
@@ -169,10 +169,10 @@ namespace triton {
         this->collectUnsymbolizedNodes(uniqueNodes, inst.getReadImmediates());
 
         /* Clean implicit and explicit semantics - MEM */
-        this->collectUnsymbolizedNodes(uniqueNodes, inst.getStoreAccess());
+        //this->collectUnsymbolizedNodes(uniqueNodes, inst.getStoreAccess());
 
         /* Clean implicit and explicit semantics - REG */
-        this->collectUnsymbolizedNodes(uniqueNodes, inst.getWrittenRegisters());
+        //this->collectUnsymbolizedNodes(uniqueNodes, inst.getWrittenRegisters());
 
         /* Clean symbolic expressions */
         for (auto it = inst.symbolicExpressions.begin(); it != inst.symbolicExpressions.end(); it++) {
